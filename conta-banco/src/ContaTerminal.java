72bfe62533ca7114c1ef82b5c84cd615cfc7db1e
Jéssica Melo <jessica.calm.dev@gmail.com>
2024-07-09 18:12:03 -0300

    Modificações na atividade Conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -7,16 +7,16 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome: ");
-        String nomeCliente = scanner.next();
+        String nomeCliente = scanner.nextLine();
 
         System.out.println("Digite o número de sua Agência com o digito verificador: ");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         System.out.println("Digite sua Conta: ");
         int numero = scanner.nextInt();
 
         System.out.println("Digite seu Saldo: ");
-        float saldo = scanner.nextFloat();
+        Double saldo = scanner.nextDouble();
 
         System.out.println(
                 "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua Agência é " + agencia
